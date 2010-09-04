02f2d9939a22ce2f20bc28d0403227e044bca9ac
Karim Osman <karim.osman@reaktor.fi>
2010-09-04 20:56:15 +0300

    fast: Add template for Login Accepted Soup TCP 2.0 message
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Mikko Korpela <mikko.korpela@reaktor.fi>

---
+++ src/fast/soup/templates/LoginAccepted.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fast.soup.templates;
+
+import fast.soup.Elements;
+import fast.templates.MessageTemplate;
+
+/** Template for Login Accepted message as specified in Soup TCP 2.0, section
+ * 2.2.1. */
+public class LoginAccepted extends MessageTemplate {
+  public static final LoginAccepted TEMPLATE = new LoginAccepted();
+
+  public LoginAccepted() {
+    add(Elements.SESSION);
+    add(Elements.SEQUENCE_NUMBER);
+  }
+}
