9c41e886103fd4b60924a6acd82c2f2212116f73
Karim Osman <karim.osman@reaktor.fi>
2010-09-04 15:41:09 +0300

    fast: Add previous value dictionary
    Also, define interface for message elements that can be used for storing
    / loading values to / from the previous value dictionary.
    
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

---
+++ src/fast/Elem.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fast;
+
+import java.nio.ByteBuffer;
+
+/** An element in a FAST message, which can be of type Presence Map, Template
+ * Identifier, or a field. */
+public interface Elem<T> {
+  T decode(final ByteBuffer buffer, final PresenceMap pmap, final Dictionary dictionary);
+}
