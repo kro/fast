df2942e0c367c264034da6894fb0735dd1cd1fe1
Karim Osman <karim.osman@reaktor.fi>
2010-09-04 21:39:13 +0300

    fast: Add BATS FAST PITCH template Order Executed (long form)
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Mikko Korpela <mikko.korpela@reaktor.fi>

---
+++ src/fast/bats/europe/templates/OrderExecutedLongForm.java
@@ -0,0 +1,5 @@
+package fast.bats.europe.templates;
+
+public class OrderExecutedLongForm extends OrderExecuted {
+  public static final OrderExecutedLongForm TEMPLATE = new OrderExecutedLongForm();
+}
