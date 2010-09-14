12625adcd07caf9afc3f9ea1c188c3fa410e390a
Mikko Korpela <mikko.korpela@reaktor.fi>
2010-09-14 16:40:44 +0300

    fast: Add test case for type decoding
    Signed-off-by: Mikko Korpela <mikko.korpela@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

---
+++ test/fast/types/TypeDecodingSpec.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fast.types;
+
+import java.nio.ByteBuffer;
+
+import jdave.Block;
+import jdave.Specification;
+import jdave.junit4.JDaveRunner;
+
+import org.junit.runner.RunWith;
+
+@RunWith(JDaveRunner.class)
+public class TypeDecodingSpec extends Specification<Type<String>> {
+  public class Initialized extends Type<String> {
+
+    private String dataString;
+    private ByteBuffer data;
+
+    public Type<String> create() {
+      dataString = "ABCDEFGHIJKLMN";
+      data = ByteBuffer.wrap(dataString.getBytes());
+      return this;
+    }
+
+    public void decodeWithMaxLimit() {
+      final int limit = dataString.length() - 5;
+      String result = decode(data, limit);
+      specify(result.length(), must.equal(limit));
+      specify(result, must.equal(dataString.substring(0, limit)));
+    }
+
+    public void decodeWithoutMaxLimitAndStopBitThrowsException() {
+      specify(new Block() {
+        @Override
+        public void run() throws Throwable {
+          decode(data);
+        }
+      }, must.raise(RuntimeException.class));
+    }
+
+    public void decodeWithStopBit() {
+      final byte[] dataArray = data.array();
+      final int stopBitIndex = dataString.length() - 3;
+      dataArray[stopBitIndex] = addStopBit(dataArray[stopBitIndex]);
+      String result = decode(data);
+      specify(result, must.equal(dataString.substring(0, stopBitIndex + 1)));
+    }
+
+    @Override
+    protected fast.types.Type.Decoder<String> createDecoder() {
+      return new Decoder<String>() {
+        private String str = "";
+
+        @Override
+        public String getResult() {
+          return str;
+        }
+
+        @Override
+        public void process(byte b) {
+          str += (char) b;
+        }
+      };
+    }
+
+    @Override
+    public ByteBuffer encode(String value) {
+      throw new RuntimeException("Only decode is tested");
+    }
+  }
+}
