1312fc7249786d2bbedadf73d2df51ed203b109f
Mikko Korpela <mikko.korpela@reaktor.fi>
2010-09-16 13:49:54 +0300

    fast: Add message encoder for Soup TCP 2.0
    Signed-off-by: Mikko Korpela <mikko.korpela@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

---
+++ src/fast/soup/SoupTCP2Encoder.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fast.soup;
+
+import java.nio.ByteBuffer;
+
+import fast.EncodeMethod;
+import fast.Encoder;
+import fast.Message;
+import fast.templates.MessageTemplate;
+
+public class SoupTCP2Encoder implements Encoder {
+  @Override
+  public ByteBuffer encode(Message message, MessageTemplate template) {
+    ByteBuffer buffer = ByteBuffer.allocate(1024);
+    template.encode(buffer, message, EncodeMethod.SOUP_TCP_2_0);
+    return buffer;
+  }
+}
