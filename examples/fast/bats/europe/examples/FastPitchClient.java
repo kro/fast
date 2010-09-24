4053421367648cf6ff769c8a033ee354ca782842
Karim Osman <karim.osman@reaktor.fi>
2010-09-24 15:51:46 +0300

    fast: Move FastPitchClient to fast.bats.europe.examples
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Mikko Korpela <mikko.korpela@reaktor.fi>

--- examples/fast/bats/europe/examples/FastPitchClient.java
+++ examples/fast/bats/europe/examples/FastPitchClient.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package fast.bats.europe.samples;
+package fast.bats.europe.examples;
 
 import java.io.FileInputStream;
 import java.io.IOException;
