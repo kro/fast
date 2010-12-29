d51b588ba0962b55a82da238a68fefd36f7604e5
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2010-12-29 13:54:56 +0200

    fast: Add setPresent to PresenceMap
    Signed-off-by: Heikki Rauhala <heikki.rauhala@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- test/fast/elements/PresenceMapSpec.java
+++ test/fast/elements/PresenceMapSpec.java
@@ -15,36 +15,76 @@
  */
 package fast.elements;
 
+import jdave.Block;
 import jdave.Specification;
 import jdave.junit4.JDaveRunner;
-
 import org.junit.runner.RunWith;
 
 @RunWith(JDaveRunner.class)
 public class PresenceMapSpec extends Specification<PresenceMap> {
   public class Initialized {
     public void validSingleByte() {
-      byte[] pMapData = { (byte) 0xc0 };
+      byte[] pMapData = {(byte) 0xc0};
       PresenceMap pMap = new PresenceMap(pMapData);
-      for(int i = 0; i < 7; i++){
+      for (int i = 0; i < 7; i++) {
         specify(pMap.isPresent(i), must.equal(i == 0));
       }
     }
 
     public void shouldReturnFalseIfIndexOutOfBounds() {
-      byte[] pMapData = { (byte) 0x80 };
+      byte[] pMapData = {(byte) 0x80};
       PresenceMap pMap = new PresenceMap(pMapData);
-      for(int i = 0; i < pMapData.length * 2; i++){
+      for (int i = 0; i < pMapData.length * 2; i++) {
         specify(pMap.isPresent(i), must.equal(false));
       }
     }
-    
+
     public void validMultiByte() {
-      byte[] pMapData = { (byte) 0x01, (byte) 0x84 };
+      byte[] pMapData = {(byte) 0x01, (byte) 0x84};
       PresenceMap pMap = new PresenceMap(pMapData);
-      for(int i = 0; i < 14; i++){
+      for (int i = 0; i < 14; i++) {
         specify(pMap.isPresent(i), must.equal(i == 6 || i == 11));
       }
+    }
+  }
+
+  public class SingleBytePresenceMap {
+    public PresenceMap create() {
+      return new PresenceMap(new byte[]{(byte) 0x80});
+    }
+
+    public void shouldIndicatePresenceAfterSetting() {
+      context.setPresent(0);
+      for (int i = 0; i < 7; i++)
+        specify(context.isPresent(i), must.equal(i == 0));
+    }
+
+    public void shouldThrowRuntimeExceptionIfIndexOutOfBounds() {
+      specify(new Block() {
+        @Override public void run() throws Throwable {
+          context.setPresent(8);
+        }
+      }, must.raise(RuntimeException.class));
+    }
+  }
+
+  public class TwoBytePresenceMap {
+    public PresenceMap create() {
+      return new PresenceMap(new byte[]{(byte) 0x0, (byte) 0x80});
+    }
+
+    public void hasPresentAfterSetting() {
+      context.setPresent(13);
+      for (int i = 0; i < 14; i++)
+        specify(context.isPresent(i), must.equal(i == 13));
+    }
+
+    public void shouldThrowRuntimeExceptionIfIndexOutOfBounds() {
+      specify(new Block() {
+        @Override public void run() throws Throwable {
+          context.setPresent(14);
+        }
+      }, must.raise(RuntimeException.class));
     }
   }
 }
